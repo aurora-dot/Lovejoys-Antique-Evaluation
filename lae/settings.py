12058fba367c2df77e4894016199c8de3281e8ea
E <e@pastel.codes>
2021-12-11 23:16:34 +0000

    Add sentry (#7)
    * Add sentry to repo

--- lae/settings.py
+++ lae/settings.py
@@ -14,6 +14,8 @@ import django_heroku
 import environ
 from pathlib import Path
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,6 +38,20 @@ DEBUG = ENV("DEBUG")
 SECRET_KEY = ENV("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = [ENV("SITENAME")]
 USE_HEROKU = ENV("USE_HEROKU")
+
+
+if not DEBUG:
+    sentry_sdk.init(
+        dsn="https://a108d751c93445f79187f5fa0008a988@o1088823.ingest.sentry.io/6103632",
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
 
 # Application definition
 
