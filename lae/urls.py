e6c5d513e6a7e26393633a387078eb97d45aa074
E <e@pastel.codes>
2021-12-13 16:28:19 +0000

    Obfuscated admin page

--- lae/urls.py
+++ lae/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("hidden/admin/obfuscated/sign/in/page/", admin.site.urls),
     path("", include("app.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
