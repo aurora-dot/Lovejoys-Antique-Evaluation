e11347a51a5481fd0b3b2cda6c158c1737cbd214
E <e@pastel.codes>
2021-12-14 16:33:06 +0000

    Add max character count to evaluation form

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.core.validators import MaxLengthValidator
 
 # Create your models here.
 
@@ -14,5 +15,5 @@ class EvaluationRequest(models.Model):
     user = models.ForeignKey(
         User, related_name="evaluation_requests", on_delete=models.CASCADE
     )
-    item_description = models.TextField()
+    item_description = models.TextField(validators=[MaxLengthValidator(5000)])
     contact_method = models.CharField(max_length=1, choices=CONTACT_METHODS)
