29c3de74ab66bd9081734a25e55624e6b3cf4da8
E <e@pastel.codes>
2021-12-14 18:22:02 +0000

    Add view requests for admin

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ app_name = "app"
 urlpatterns = [
     path("", views.index, name="index"),
     path("request_evaluation/", views.request_evaluation, name="request_evaluation"),
+    path("view_requests/", views.admin_view_requests, name="admin_view_requests"),
 ]
