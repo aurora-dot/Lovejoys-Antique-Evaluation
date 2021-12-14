a15707399f65dc1118c3dd94598bd808c979c18e
E <e@pastel.codes>
2021-12-14 22:40:42 +0000

    Fix signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 from phonenumber_field.formfields import PhoneNumberField
-from django import forms
 
 
 class UserCreationForm(UserCreationForm):
     phone_number = PhoneNumberField(required=True)
-    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name")
+        fields = ("username", "email", "first_name", "last_name")
