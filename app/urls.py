613770cb224382484defa53adfc092c522c870aa
E <e@pastel.codes>
2021-12-14 14:51:17 +0000

    Add initial request evaluation template and view

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "app"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("request_evaluation/", views.request_evaluation, name="request_evaluation"),
 ]
--- app/views.py
+++ app/views.py
@@ -8,3 +8,7 @@ def index(request):
     # send_mail('subject', 'body of the message',
     # 'no-reply@lovejoyantiques.xyz', ['user@example.com'])
     return render(request, "app/index.html")
+
+
+def request_evaluation(request):
+    return render(request, "app/request_evaluation.html")
