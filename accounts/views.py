4e8943dc609438cf0690889a475bc024c1249c3e
E <e@pastel.codes>
2021-12-15 08:41:57 +0000

    Fix form views

--- accounts/views.py
+++ accounts/views.py
@@ -9,12 +9,16 @@ def signup_view(request):
     if request.user.is_authenticated:
         return redirect("/")
 
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password1")
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect("/")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+
     return render(request, "accounts/signup.html", {"form": form})
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.http import Http404
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from .forms import EvaluationForm
 from accounts.models import User
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -9,22 +10,25 @@ def index(request):
     return render(request, "app/index.html")
 
 
+@login_required(login_url="/accounts/login/")
 def request_evaluation(request):
-    if request.user.is_authenticated:
-        submitted = False
+    submitted = False
+
+    if request.method == "POST":
         form = EvaluationForm(request.POST, request.FILES)
         if form.is_valid():
             obj = form.save(commit=False)
             obj.user = User.objects.get(pk=request.user.id)
             obj.save()
             submitted = True
-        return render(
-            request,
-            "app/request_evaluation.html",
-            {"form": form, "submitted": submitted},
-        )
     else:
-        return redirect("/")
+        form = EvaluationForm
+
+    return render(
+        request,
+        "app/request_evaluation.html",
+        {"form": form, "submitted": submitted},
+    )
 
 
 def admin_view_requests(request):
