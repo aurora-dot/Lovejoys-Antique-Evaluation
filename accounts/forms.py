80e252abe72973feda374eb84bf3f8754d64ed65
E <e@pastel.codes>
2021-12-15 08:29:06 +0000

    Remove redundant field in signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
-from phonenumber_field.formfields import PhoneNumberField
 
 
 class UserCreationForm(UserCreationForm):
-    phone_number = PhoneNumberField(required=True)
-
     class Meta:
         model = User
         fields = ("username", "email", "phone_number", "first_name", "last_name")
