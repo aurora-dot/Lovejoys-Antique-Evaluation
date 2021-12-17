6a5e2237ef2c95e12b7994d4491217220aa57877
E <e@pastel.codes>
2021-12-17 07:51:39 +0000

    Attempt hcaptcha

--- lae/settings.py
+++ lae/settings.py
@@ -207,7 +207,7 @@ PASSWORD_HASHERS = [
 if not DEBUG:
     CSP_DEFAULT_SRC = "'none'"
     CSP_STYLE_SRC = "'self'"
-    CSP_SCRIPT_SRC = "'self'"
+    CSP_SCRIPT_SRC = ("'self'", "js.hcaptcha.com")
     CSP_FONT_SRC = "'self'"
     CSP_IMG_SRC = ("'self'", "lovejoy-antique-media.s3.amazonaws.com")
 
